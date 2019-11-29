f227290ea9535ad26f4b306c8287c3458da47000
MichaelNewman <newms226@gmail.com>
2019-11-28 19:21:53 -0700

    ...but apparently I dont know how to commit everything at once

--- CarShop-API/app/controllers/typeahead_controller.rb
+++ CarShop-API/app/controllers/typeahead_controller.rb
@@ -3,7 +3,16 @@ class TypeaheadController < ApplicationController
     @makes = Make.all.pluck(:name)
     @models = Car.all.pluck(:model).uniq
     @parts = Part.all.pluck(:name)
-    @vins = Car.all.pluck(:vin)
+
+    vins = Car.all.pluck(:vin)
+    prev = vins[0];
+    @vins = vins.slice_before { |e|
+      prev, prev2 = e, prev
+      prev2 + 1 != e
+    }.map { |es| [es.first, es.last] }
+
+    # p vins
+    # render json: vins
     render 'typeahead/cars.json', format: :json
   end
 
