6180cfb81df57be383667010a1381d3575746bd8
MichaelNewman <newms226@gmail.com>
2019-12-11 14:55:36 -0700

    login component input update and view update

--- CarShop-Spa/src/app/authn/login/login.component.ts
+++ CarShop-Spa/src/app/authn/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Optional, Input } from '@angular/core';
 import { AngularTokenService } from 'angular-token';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
@@ -10,8 +10,11 @@ import { AuthnService } from '../authn.service';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  @Input() errorMessage: string;
+  @Input() email: string;
+
   loginForm = new FormGroup({
-    'login': new FormControl('', [
+    'login': new FormControl(this.email || '', [
       Validators.email,
       Validators.required
     ]),
@@ -20,8 +23,6 @@ export class LoginComponent implements OnInit {
     ])
   });
 
-  private invalidCredentials: boolean = false;
-  attemptSubmission: boolean = false;
 
   constructor(public activeModal: NgbActiveModal, private authn: AuthnService) { }
 
@@ -29,46 +30,20 @@ export class LoginComponent implements OnInit {
     
   }
 
-  private get login() { return this.loginForm.get('login'); }
-  private get password() { return this.loginForm.get('password'); }
-
-  get isInvalid() { 
-    return this.attemptSubmission && 
-      (this.invalidCredentials || this.login.errors || this.password.errors) 
-  }
-
-  getErrorMessage() {
-    // this.attemptSubmission = false;
-
-    if (this.login.errors) {
-      if (this.login.errors.required) {
-        return 'Must supply email.';
-      } else if (this.login.errors.email) {
-        return 'Login must be a valid email.';
-      }
-    } else if (this.password.errors && this.password.errors.required) {
-      return 'Must provide a password.';
-    } else if (this.invalidCredentials) {
-      return 'Invalid email/password.';
-    }
-  }
-
   submitLogin() {
-    this.attemptSubmission = true;
-
-    if (!this.login.errors && !this.password.errors) {
+    // if (!this.login.errors && !this.password.errors) {
       this.authn.signIn(this.loginForm.value).subscribe(
         _ => {
           console.log('Signed in! :)'); 
           this.activeModal.dismiss('Cross click');
         },
         err => {
-          this.invalidCredentials = true;
           console.log('Failed to log in: '); 
           console.log(err);
+          this.errorMessage = 'Invalid login credentials.'
         }
       )
-    }
+    // }
   }
 
 }
