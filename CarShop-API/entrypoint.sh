460f8ef487a9e249c5fb0e4bfaf6c7f07168a354
MichaelNewman <newms226@gmail.com>
2019-12-20 10:04:55 -0700

    this apparenty works?

--- CarShop-API/entrypoint.sh
+++ CarShop-API/entrypoint.sh
@@ -6,7 +6,7 @@ set -e
 
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pid
-bundle install
+# bundle install
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
